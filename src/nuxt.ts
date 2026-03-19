d0bbaf81f2167648182347f81926b441a7d3f27d
shivakumar <kumar@appz.dev>
2026-03-19 13:50:07 +0000

    fix(pageflare): fix Nuxt DTS generation with portable type annotation

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,8 +1,5 @@
-import { defineNuxtModule, type useNitro, type useNuxt } from "@nuxt/kit";
+import { defineNuxtModule } from "@nuxt/kit";
 import { optimize, type PageflarePluginOptions } from "./core.js";
-
-type Nuxt = ReturnType<typeof useNuxt>;
-type Nitro = ReturnType<typeof useNitro>;
 
 export default defineNuxtModule<PageflarePluginOptions>({
 	meta: {
@@ -10,8 +7,9 @@ export default defineNuxtModule<PageflarePluginOptions>({
 		configKey: "pageflare",
 	},
 	defaults: {},
-	setup(options: PageflarePluginOptions, nuxt: Nuxt) {
-		nuxt.hook("nitro:init", (nitro: Nitro) => {
+	setup(options, nuxt) {
+		// biome-ignore lint/suspicious/noExplicitAny: Nitro types reference @nuxt/schema which isn't portable for DTS
+		nuxt.hook("nitro:init", (nitro: any) => {
 			nitro.hooks.hook("close", async () => {
 				const outputDir = nitro.options.output.publicDir;
 				await optimize({
